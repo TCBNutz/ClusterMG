1f09050defa1a94939f98c83380776a98642f8c4
Pete Shadbolt <pete.shadbolt@gmail.com>
2014-12-08 01:38:06 +0000

    Investigating size/duplication in auxN matrices

--- auxiliary_old.py
+++ auxiliary_old.py
@@ -2,7 +2,6 @@
 import itertools
 import numpy as np
 from sarmaH import *
-import pauli
 
 """ function measurement returns state of the unmeasured qubits after projective measurement
 onto |state> state of qubit at position position. Initial state given by dmat."""
@@ -30,61 +29,68 @@ def measurement(dmat,position,state):
 """ function dmat gives full Emitter x PhotonString x Environment density matrix. ph is number of photons, Omega is
 Zeeman energy of emitter, wlist is list of Zeeman energies of environment spins, Alist is list of hyperfine couplings"""
 def dmat(ph,Omega,wlist,Alist,bmat,envinit):
-    pdim=2**ph
     envdim=2**len(Alist) #environment dimension
     OmegaEff=Omega + 0.25*sum(Alist**2)/Omega #effective magnetic field, <Overhauser> = 0
+
     HamiltonianPD=STot(len(Alist),Alist,wlist,bmat,Omega) #pure dephasing Hamiltonian
+
     eigsys=np.linalg.eigh(HamiltonianPD) #(array([eig.val.1, eig.val.2,...]), array([eigenvector1,eigenvector2,...]))
+
     Udiag=np.diag(np.exp(-1j*0.5*np.pi*eigsys[0]/OmegaEff)) #propagator for t/hbar = Pi/(2 OmegaEff) in eigenbasis
+
     Unum=np.dot(eigsys[1],np.dot(Udiag,np.conj(eigsys[1].T))) #propagator in numberbasis
 
     #environment operators
-    A=np.array([[Unum[:envdim,:envdim],Unum[:envdim,envdim:]],[Unum[envdim:,:envdim],Unum[envdim:,envdim:]]])
+    A=np.array([[Unum[0:envdim,0:envdim],Unum[0:envdim,envdim:]],[Unum[envdim:,0:envdim],Unum[envdim:,envdim:]]])
 
     #list of matrices Fb giving the environment operators F(b) for each bit string b
-    b=np.array(list(itertools.product([0,1],repeat=ph)))
-    Fb=np.ones((pdim, envdim, envdim), dtype=complex)
-    identity=np.eye(envdim)
-    #TODO: there must be a more efficient construction here 
-    for j in range(pdim):
-        for i in range(ph): # For each photon
+    b=list(itertools.product([0,1],repeat=ph))
+    Fb=[1]*2**ph
+    identity=np.diag([1]*envdim)
+
+    for j in range(2**ph):
+        for i in range(ph):
             if i==0:
                 Fb[j]=np.dot(A[b[j][ph-1],0],identity)
             else:
-                Fb[j]=np.dot(A[b[j][ph-i-1], b[j][ph-i]], Fb[j]) # Act the operator for each photon
+                Fb[j]=np.dot(A[b[j][ph-i-1],b[j][ph-i]],Fb[j])
 
     # making aux2 as a matrix such that every column is the state of emitter and photonic bit string
-    # Note that aux1, aux2 and aux3 depend only on the number of photons.
-    b=np.array(list(itertools.product([0,1],repeat=ph)))
-    aux2=np.zeros((pdim, 2**(ph+1), 1))
-    aux1=np.vstack((b[:,0]==0, b[:,0]!=0))
-    for i in range(pdim):
-        aux3=np.array([[0]]*pdim)
+    aux1=np.array([[0]*2**ph,[0]*2**ph])
+    aux2=np.array([[[0]]*2**(ph+1)]*2**ph)
+    for i in range(2**ph):
+        if b[i][0]==0:
+            aux1[0,i]=1
+        else:
+            aux1[1,i]=1        
+    for i in range(2**ph):
+        aux3=np.array([[0]]*2**ph)
         aux3[i,0]=1
         aux2[i]=np.kron(aux1[:,[i]],aux3)
-    # aux1, aux2 and aux3 are very sparse binary matrices. there must be a good optimization
-    print aux2[:,:,0]
-    print aux1
-
-    #aux2=np.hstack(np.kron(aux1[:, [i]], 
 
     """full density matrix dmat"""
-    d=2**(ph+1)*envdim
-    dmatCn=np.zeros((d,d), dtype=complex)
-    for i in range(pdim):
-        for j in range(pdim):
+    dmatCn=np.array([[0.+0.J]*2**(ph+1)*envdim]*2**(ph+1)*envdim)
+    for i in range(2**ph):
+        for j in range(2**ph):
             dmatCn=dmatCn+np.kron(np.kron(aux2[i],aux2[j].T[0]),np.dot(Fb[i],np.dot(envinit,np.conj(Fb[j].T))))
 
     """ dmat gives the (approximation to) the state |C_n> (eq. 1 in Dara's paper). To make this a Cluster
     state we must rotate once more and then apply a Z-gate to each photon"""
 
     """Uph is the propagator for Pi/2 rotation in the emitter + photon string + environment number basis """
-    phidentity=np.eye(pdim) #identity on photon string Hilbert space
+    phidentity=np.diag([1.]*2**ph) #identity on photon string Hilbert space
     Uph=np.kron(np.array([[1,0],[0,0]]),np.kron(phidentity,A[0,0]))+\
     np.kron([[0,1],[0,0]],np.kron(phidentity,A[0,1]))+np.kron([[0,0],[1,0]],np.kron(phidentity,A[1,0]))+ \
     np.kron([[0,0],[0,1]],np.kron(phidentity,A[1,1]))
 
     """Z-gate on each photon"""
-    Zph=reduce(np.kron, (pauli.sz for i in xrange(ph)))
+    PauliZ=np.array([[1,0],[0,-1]])
+    Zphi=[0,PauliZ]
+    for i in range(ph-1):
+        Zphi[0]=np.kron(Zphi[1],PauliZ)
+        Zphi[1]=Zphi[0]
+
+    Zph=Zphi[0] # this is Z x Z x ... Z (ph times)
     ZphBig=np.kron(np.array([[1,0],[0,1]]),np.kron(Zph,identity))
+
     return np.dot(np.dot(ZphBig,Uph),np.dot(dmatCn,np.conj(np.dot(ZphBig,Uph).T)))
--- densitymatrix.py
+++ densitymatrix.py
@@ -1,60 +1,36 @@
 import numpy as np
 import pauli
 import auxiliary as aux
+import auxiliary_old as aux_old
 from sarmaH import *
 import itertools
+from time import clock
 
-def main():
-    """ setting parameters """
-    ph=4 #number of photons
-    Alist=np.array([0,0,0,0]) #hyperfine coefficients
-    wlist=np.array([0,0,0,0]) #nuclear Zeeman
-    bmat=np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]) #dipolar coupling
-    Omega=15 #emitter Zeeman
+""" setting parameters """
+ph=2 #number of photons
+Alist=np.array([0,0,0,0]) #hyperfine coefficients
+wlist=np.array([0,0,0,0]) #nuclear Zeeman
+bmat=np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]) #dipolar coupling
+Omega=15 #emitter Zeeman
 
-    #initial unpolarized state of environment, the ugly way
-    yp, ym=pauli.yp, pauli.ym
-    mkron = lambda x: reduce(np.kron, x)
-    envinit=1.0/6.0*(np.outer(np.conj(mkron((yp,yp,ym,ym)).T), mkron((yp,yp,ym,ym)))+ \
-                     np.outer(np.conj(mkron((yp,ym,ym,yp)).T), mkron((yp,ym,ym,yp)))+ \
-                     np.outer(np.conj(mkron((ym,ym,yp,yp)).T), mkron((ym,ym,yp,yp)))+ \
-                     np.outer(np.conj(mkron((yp,ym,yp,ym)).T), mkron((yp,ym,yp,ym)))+ \
-                     np.outer(np.conj(mkron((ym,yp,ym,yp)).T), mkron((ym,yp,ym,yp)))+ \
-                     np.outer(np.conj(mkron((ym,yp,yp,ym)).T), mkron((ym,yp,yp,ym))))
-
-
-    dmat=aux.dmat(ph,Omega,wlist,Alist,bmat,envinit)
-
-    # tracing over the environment to obtain the reduced density matrix dmatred"""
-    """
-    dmatred=np.array([[0.+0.j]*2**(ph+1)]*2**(ph+1))
-    envdim=2**len(Alist)
-    for m in range(2**(ph+1)):
-        for n in range(2**(ph+1)):
-            dmatred[m,n]=np.trace(aux.dmat(ph,Omega,wlist,Alist,bmat,envinit)[m*envdim:(m+1)*envdim,n*envdim:(n+1)*envdim])"""
-
-    #print(dmat)
-    print "finished"
-
-if __name__ == '__main__':
-    from time import clock
-    import anydbm
-    from datetime import datetime
-    from matplotlib import pyplot as plt
-    starttime=clock()
-    main()
-    elapsedtime=clock()-starttime
-
-    # Make a database entry and plot stuff
-    now=datetime.now().ctime()
-    db = anydbm.open("progress.db", "c")
-    db[str(now)]=str(elapsedtime)
-    everything=map(lambda x: [datetime.strptime(x[0], '%a %b %d %H:%M:%S %Y'), x[1]], db.items())
-    everything=sorted(everything, key=lambda x: x[0])
-    times, counts=zip(*everything)
-    plt.plot_date(times, counts, '.-', lw=.5, color='black')
-    plt.xlabel('Date'); plt.ylabel('Time elapsed'); plt.ylim(0,3)
-    plt.savefig("progress.pdf")
+#initial unpolarized state of environment, the ugly way
+yp, ym=pauli.yp, pauli.ym
+mkron = lambda x: reduce(np.kron, x)
+envinit=1.0/6.0*(np.outer(np.conj(mkron((yp,yp,ym,ym)).T), mkron((yp,yp,ym,ym)))+ \
+                 np.outer(np.conj(mkron((yp,ym,ym,yp)).T), mkron((yp,ym,ym,yp)))+ \
+                 np.outer(np.conj(mkron((ym,ym,yp,yp)).T), mkron((ym,ym,yp,yp)))+ \
+                 np.outer(np.conj(mkron((yp,ym,yp,ym)).T), mkron((yp,ym,yp,ym)))+ \
+                 np.outer(np.conj(mkron((ym,yp,ym,yp)).T), mkron((ym,yp,ym,yp)))+ \
+                 np.outer(np.conj(mkron((ym,yp,yp,ym)).T), mkron((ym,yp,yp,ym))))
 
 
+t=clock()
+dmat=aux.dmat(ph,Omega,wlist,Alist,bmat,envinit)
+t1=clock()-t; t=clock()
+dmat2=aux_old.dmat(ph,Omega,wlist,Alist,bmat,envinit)
+t2=clock()-t
 
+if np.allclose(dmat, dmat2):
+    print "success: %.4f" % (t1/t2)
+else:
+    print "fail"
